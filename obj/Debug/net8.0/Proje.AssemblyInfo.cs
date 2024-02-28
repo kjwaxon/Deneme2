3f15ce0d208a322c125816a0f98ca9c3ac2d1ec9
kjwaxon <kjwaxon@gmail.com>
2024-02-28 15:06:33 +0300

    UpdateProgramCS

--- obj/Debug/net8.0/Proje.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7392633d7ef159fc5a4640992e57d6921c9346b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
